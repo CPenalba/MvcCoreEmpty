b2756c607ba1e08844a37f6d849eb10d4610cae9
CPenalba <carolinapenalbacorpa@gmail.com>
2025-01-30 11:07:27 +0100

    Bootstrap

--- MvcCoreEmpty/Program.cs
+++ MvcCoreEmpty/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+//TODO LO QUE INDIQUEMOS SERAN SERVICIOS
+//AÑADIMOS LOS SERVICIOS DE VISTAS Y CONTROLLERS
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//DEBEMOS INDICAR QUE UTILIZAREMOS RUTAS PARA 
+//CONTROLLERS Y VIEWS
+
+app.UseStaticFiles(); 
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}"
+);
 
 app.Run();
